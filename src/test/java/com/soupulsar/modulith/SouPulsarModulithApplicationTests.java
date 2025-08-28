12855becf969c283acc4371e33983f87a9cfbef0
Mateus Ribeiro <mateusribeiro-2004@hotmail.com>
2025-08-28 17:22:51 -0300

    initial commit

---
+++ src/test/java/com/soupulsar/modulith/SouPulsarModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soupulsar.modulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouPulsarModulithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
